a99175a1770b35f280f8883fdb2a2040fabe0824
Julai Dangol <99350702+julaij@users.noreply.github.com>
2024-03-26 09:23:08 -0500

    add artists object and render on localhost

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Node Express Pug Project
 [Render Server Link](https://s24wb71dangol.onrender.com/)
 
 Class : Artist
-Attributes : artist_name, artist_age, artist_masterpiece
+Attributes : artist_name, artist_country, artist_masterpiece
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var artistsRouter = require('./routes/artist')
+var artistsRouter = require('./routes/artists')
 
 var app = express();
 
